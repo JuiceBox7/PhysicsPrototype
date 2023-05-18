7dafafa5b4fd95c14aeb8f0bfdc5a381c15595da
JuiceBox7 <juiceinbox7@gmail.com>
2023-05-17 23:47:34 -0700

    Finished designing levels and added audio

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # PhysicsPrototype
-Class assignment for a mini physics project using Phaser 3
 
+A mini platforming challenge made for a class assignment using Phaser 3
+Play here
+
+## Gameplay/Experience Requirements:
+- Uses both continuous and discrete inputs from the player: Satisfied
+- The player's goal can only be achieved indirectly: Satisfied
+- 3+ physics-based gameplay scenes: Satisfied
+- Other scenes are used to separate and contextualize the gameplay scenes
+
+## Images and Audio:
+All images and sounds made by me using:
+[Pixilart](https://pixilart.com/)
+[Sfxr](https://sfxr.me/)
+[Beepbox](https://www.beepbox.co/#9n31s0k0l00e03t2ma7g0fj07r1i0o432T7v1u32f10p5q011d08HU7000U0006000Eh0I9E0T7v1u38f20o5134q011d0aHt7760Md9xb9pb9h1IcE0T1v1uc3f10v3q011d23A1F0B2Q0950Pc454E26327aT2v1u15f10w4qw02d03w0E0b4h400000000h4g000000014h000000004h400000000p16000000)
 
 ## Code References:
 https://labs.phaser.io/edit.html?src=src%5Cscenes%5Cpassing%20data%20to%20a%20scene.js
 https://medium.com/@alizah.lalani/collecting-objects-in-phaser-3-platformer-games-using-tiled-4e9298cbfc85
 https://github.com/photonstorm/phaser3-examples/blob/master/public/src/physics/arcade/variable%20step.js
+https://workshops.nuevofoundation.org/phaser-space-invaders-game/activity-6/
--- script.js
+++ script.js
@@ -24,49 +24,78 @@ class Level1 extends Phaser.Scene {
         this.load.image('dave', 'assets/dave.png')
         this.load.image('arrowKeys', 'assets/arrow-keys.png')
         this.load.image('diamond', 'assets/diamond.png')
-        // load tile map
+        this.load.image('levelBackground', 'assets/level-background.png')
+        this.load.image('block', 'assets/block.png')
+        this.load.audio('jump', 'assets/jump.wav')
+        this.load.audio('pickup', 'assets/pickup.wav')
+        this.load.audio('bgm', 'assets/bgm.wav')
     }
 
     collect(dave, diamond) {
         diamond.destroy(diamond.x, diamond.y)
+        this.pickup.play()
         this.diamondCount += 1
         this.score.setText('Diamonds: ' + this.diamondCount)
         if (this.diamondCount == 1) {
+            this.bgm.stop()
             this.scene.start('summary', {time: this.game.getTime() - this.startTime, score: this.diamondCount, level: 1})
         }
     }
 
+    reset() {
+        this.dave.disableBody(true, true)
+        this.dave.enableBody(true, 1, game.config.height/1.5, true, true)
+    }
+
     create() {
-        this.cameras.main.setBackgroundColor('#227B96')
+        this.add.image(400, 300, 'levelBackground')
         this.startTime = this.game.getTime()
-        // add tile map
         this.physics.world.gravity.y = 3000
-        this.ground = this.physics.add.sprite(game.config.width/2, game.config.height, 'dave').setScale(0.5)
-        this.dave = this.physics.add.sprite(game.config.width/2, game.config.height/2, 'dave').setScale(0.25)
+   
+        this.pickup = this.sound.add('pickup').setVolume(0.25)
+        this.jump = this.sound.add('jump').setVolume(0.25)
+        this.bgm = this.sound.add('bgm').setVolume(0.3)
+        this.bgm.play()
+
+        this.platforms = this.physics.add.staticGroup()
+        this.platforms.create(40, 560, 'block').setScale(0.05).refreshBody()
+        this.platforms.create(120, 560, 'block').setScale(0.05).refreshBody()
+        this.platforms.create(200, 560, 'block').setScale(0.05).refreshBody()
+        this.platforms.create(280, 560, 'block').setScale(0.05).refreshBody()
+        this.platforms.create(360, 560, 'block').setScale(0.05).refreshBody()
+        this.platforms.create(620, 560, 'block').setScale(0.05).refreshBody()
+        this.platforms.create(700, 560, 'block').setScale(0.05).refreshBody()
+        this.platforms.create(780, 560, 'block').setScale(0.05).refreshBody()
+        this.platforms.create(860, 560, 'block').setScale(0.05).refreshBody()
+        this.platforms.create(940, 560, 'block').setScale(0.05).refreshBody()
+        this.platforms.create(1020, 560, 'block').setScale(0.05).refreshBody()
+
+        this.pit = this.physics.add.staticGroup()
+        this.pit.create(440, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+        this.pit.create(520, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+
+        this.dave = this.physics.add.sprite(1, game.config.height/1.5, 'dave').setScale(0.2)
         
-        this.diamond = this.physics.add.sprite(550, 250, 'diamond')
-            .setScale(0.25)
+        this.diamond = this.physics.add.sprite(950, 475, 'diamond')
+            .setScale(0.075)
             .setImmovable(true)
         this.diamond.body.setAllowGravity(false)
 
         this.diamondCount = 0
 
-        this.score = this.add.text(620, 25, 'Diamonds: ' + this.diamondCount, {
+        this.score = this.add.text(820, 25, 'Diamonds: ' + this.diamondCount, {
             fontSize: '20px',
             fill: 0xFFFFFF
         })
         this.score.setScrollFactor(0)
 
-        this.ground.setCollideWorldBounds(true)
         this.dave.setCollideWorldBounds(true)
         this.diamond.setCollideWorldBounds(true)
-        this.physics.add.collider(this.diamond, this.ground)
-        this.physics.add.collider(this.dave, this.ground)
+        this.physics.add.collider(this.dave, this.platforms)
         this.physics.add.overlap(this.dave, this.diamond, this.collect, null, this)
+        this.physics.add.overlap(this.dave, this.pit, this.reset, null, this)
 
         cursors = this.input.keyboard.createCursorKeys()
-        
-        // this.physics.add.collider(this.dave, this.ground)
 
         // arrow keys ui
         this.add.sprite(65, 45, 'arrowKeys')
@@ -88,6 +117,7 @@ class Level1 extends Phaser.Scene {
         }
         
         if (this.dave.body.touching.down && Phaser.Input.Keyboard.JustDown(cursors.up)) {
+            this.jump.play()
             this.dave.body.setVelocityY(-1000)
         }
 
@@ -102,11 +132,115 @@ class Level2 extends Phaser.Scene {
 
     preload() {
         this.load.image('dave', 'assets/dave.png')
-        // load tile map
+        this.load.image('arrowKeys', 'assets/arrow-keys.png')
+        this.load.image('diamond', 'assets/diamond.png')
+        this.load.image('levelBackground', 'assets/level-background.png')
+        this.load.image('block', 'assets/block.png')
+        this.load.audio('jump', 'assets/jump.wav')
+        this.load.audio('pickup', 'assets/pickup.wav')
+        this.load.audio('bgm', 'assets/bgm.wav')
+    }
+
+    collect(dave, diamond) {
+        diamond.destroy(diamond.x, diamond.y)
+        this.pickup.play()
+        this.diamondCount += 1
+        this.score.setText('Diamonds: ' + this.diamondCount)
+        if (this.diamondCount == 2) {
+            this.bgm.stop()
+            this.scene.start('summary', {time: this.game.getTime() - this.startTime, score: this.diamondCount, level: 2})
+        }
+    }
+
+    reset() {
+        this.dave.disableBody(true, true)
+        this.dave.enableBody(true, 1, game.config.height/1.5, true, true)
     }
 
     create() {
+        this.add.image(400, 300, 'levelBackground')
+        this.startTime = this.game.getTime()
+        this.physics.world.gravity.y = 3000
 
+        this.pickup = this.sound.add('pickup').setVolume(0.25)
+        this.jump = this.sound.add('jump').setVolume(0.25)
+        this.bgm = this.sound.add('bgm').setVolume(0.3)
+        this.bgm.play()
+
+        this.platforms = this.physics.add.staticGroup()
+        this.platforms.create(40, 560, 'block').setScale(0.05).refreshBody()
+        this.platforms.create(120, 560, 'block').setScale(0.05).refreshBody()
+        this.platforms.create(300, 400, 'block').setScale(0.05).refreshBody()
+        this.platforms.create(530, 550, 'block').setScale(0.025).refreshBody()
+        this.platforms.create(750, 500, 'block').setScale(0.025).refreshBody()
+        this.platforms.create(940, 560, 'block').setScale(0.05).refreshBody()
+        this.platforms.create(1020, 560, 'block').setScale(0.05).refreshBody()
+
+        this.pit = this.physics.add.staticGroup()
+        this.pit.create(200, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+        this.pit.create(280, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+        this.pit.create(360, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+        this.pit.create(440, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+        this.pit.create(520, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+        this.pit.create(600, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+        this.pit.create(680, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+        this.pit.create(760, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+        this.pit.create(840, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+
+        this.dave = this.physics.add.sprite(1, game.config.height/1.5, 'dave').setScale(0.2)
+        
+        this.diamond = this.physics.add.sprite(950, 475, 'diamond')
+            .setScale(0.075)
+            .setImmovable(true)
+        this.diamond.body.setAllowGravity(false)
+
+        this.diamond2 = this.physics.add.sprite(530, 300, 'diamond')
+            .setScale(0.075)
+            .setImmovable(true)
+        this.diamond2.body.setAllowGravity(false)
+
+        this.diamondCount = 0
+
+        this.score = this.add.text(820, 25, 'Diamonds: ' + this.diamondCount, {
+            fontSize: '20px',
+            fill: 0xFFFFFF
+        })
+        this.score.setScrollFactor(0)
+
+        this.dave.setCollideWorldBounds(true)
+        this.diamond.setCollideWorldBounds(true)
+        this.physics.add.collider(this.dave, this.platforms)
+        this.physics.add.overlap(this.dave, this.diamond, this.collect, null, this)
+        this.physics.add.overlap(this.dave, this.diamond2, this.collect, null, this)
+        this.physics.add.overlap(this.dave, this.pit, this.reset, null, this)
+
+        cursors = this.input.keyboard.createCursorKeys()
+
+        // arrow keys ui
+        this.add.sprite(65, 45, 'arrowKeys')
+            .setScale(0.25)
+            .setTint(0x000000)
+    }
+
+    update() {
+        if (cursors.left.isDown) {
+            this.dave.setVelocityX(-300)
+        }
+
+        else if (cursors.right.isDown) {
+            this.dave.setVelocityX(300)
+        }
+
+        else {
+            this.dave.body.velocity.x = 0
+        }
+        
+        if (this.dave.body.touching.down && Phaser.Input.Keyboard.JustDown(cursors.up)) {
+            this.jump.play()
+            this.dave.body.setVelocityY(-1000)
+        }
+
+        this.physics.world.wrap(this.dave, this.dave.width/2)
     }
 }
 
@@ -117,11 +251,125 @@ class Level3 extends Phaser.Scene {
 
     preload() {
         this.load.image('dave', 'assets/dave.png')
-        // load tile map
+        this.load.image('arrowKeys', 'assets/arrow-keys.png')
+        this.load.image('diamond', 'assets/diamond.png')
+        this.load.image('levelBackground', 'assets/level-background.png')
+        this.load.image('block', 'assets/block.png')
+        this.load.audio('jump', 'assets/jump.wav')
+        this.load.audio('pickup', 'assets/pickup.wav')
+        this.load.audio('bgm', 'assets/bgm.wav')
+    }
+
+    collect(dave, diamond) {
+        diamond.destroy(diamond.x, diamond.y)
+        this.pickup.play()
+        this.diamondCount += 1
+        this.score.setText('Diamonds: ' + this.diamondCount)
+        if (this.diamondCount == 3) {
+            this.bgm.stop()
+            this.scene.start('summary', {time: this.game.getTime() - this.startTime, score: this.diamondCount, level: 3})
+        }
+    }
+
+    reset() {
+        this.dave.disableBody(true, true)
+        this.dave.enableBody(true, 1, game.config.height/1.5, true, true)
     }
 
     create() {
+        this.add.image(400, 300, 'levelBackground')
+        this.startTime = this.game.getTime()
+        this.physics.world.gravity.y = 3000
 
+        this.pickup = this.sound.add('pickup').setVolume(0.25)
+        this.jump = this.sound.add('jump').setVolume(0.25)
+        this.bgm = this.sound.add('bgm').setVolume(0.3)
+        this.bgm.play()
+
+        this.platforms = this.physics.add.staticGroup()
+        this.platforms.create(40, 560, 'block').setScale(0.05).refreshBody()
+        this.platforms.create(150, 400, 'block').setScale(0.025).refreshBody()
+        this.platforms.create(280, 550, 'block').setScale(0.025).refreshBody()
+        this.platforms.create(280, 280, 'block').setScale(0.025).refreshBody()
+        this.platforms.create(500, 150, 'block').setScale(0.025).refreshBody()
+        this.platforms.create(750, 500, 'block').setScale(0.025).refreshBody()
+        this.platforms.create(480, 360, 'block').setScale(0.025).refreshBody()
+        this.platforms.create(940, 560, 'block').setScale(0.05).refreshBody()
+        this.platforms.create(1020, 560, 'block').setScale(0.05).refreshBody()
+
+        this.pit = this.physics.add.staticGroup()
+        this.pit.create(200, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+        this.pit.create(280, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+        this.pit.create(360, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+        this.pit.create(440, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+        this.pit.create(520, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+        this.pit.create(600, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+        this.pit.create(680, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+        this.pit.create(760, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+        this.pit.create(840, 639, 'block').setScale(0.05).refreshBody().setVisible(false)
+
+        this.dave = this.physics.add.sprite(1, game.config.height/1.5, 'dave').setScale(0.2)
+        
+        this.diamond = this.physics.add.sprite(950, 475, 'diamond')
+            .setScale(0.075)
+            .setImmovable(true)
+        this.diamond.body.setAllowGravity(false)
+
+        this.diamond2 = this.physics.add.sprite(525, 75, 'diamond')
+            .setScale(0.075)
+            .setImmovable(true)
+        this.diamond2.body.setAllowGravity(false)
+
+        this.diamond3 = this.physics.add.sprite(280, 515, 'diamond')
+            .setScale(0.075)
+            .setImmovable(true)
+        this.diamond3.body.setAllowGravity(false)
+
+        this.diamondCount = 0
+
+        this.score = this.add.text(820, 25, 'Diamonds: ' + this.diamondCount, {
+            fontSize: '20px',
+            fill: 0xFFFFFF
+        })
+        this.score.setScrollFactor(0)
+
+        this.dave.setCollideWorldBounds(true)
+        this.diamond.setCollideWorldBounds(true)
+        this.physics.add.collider(this.dave, this.platforms)
+        this.physics.add.overlap(this.dave, this.diamond, this.collect, null, this)
+        this.physics.add.overlap(this.dave, this.diamond2, this.collect, null, this)
+        this.physics.add.overlap(this.dave, this.diamond3, this.collect, null, this)
+        this.physics.add.overlap(this.dave, this.pit, this.reset, null, this)
+
+        cursors = this.input.keyboard.createCursorKeys()
+
+        // arrow keys ui
+        this.add.sprite(65, 45, 'arrowKeys')
+            .setScale(0.25)
+            .setTint(0x000000)
+    }
+
+    update() {
+        if (cursors.left.isDown) {
+            this.dave.body.setAccelerationX(-500)
+
+        }
+
+        else if (cursors.right.isDown) {
+            this.dave.body.setAccelerationX(500)
+        }
+
+        else {
+            this.dave.body.setAccelerationX(0)
+            this.dave.body.setDragX(200)
+        }
+        
+        if (this.dave.body.touching.down && Phaser.Input.Keyboard.JustDown(cursors.up)) {
+            this.jump.play()
+            this.dave.body.setVelocityY(-1000)
+        }
+
+        this.physics.world.wrap(this.dave, this.dave.width/2)
     }
 }
 
@@ -137,15 +385,32 @@ class Summary extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('background', 'assets/background.png')
+        this.load.image('summaryBackground', 'assets/summary-background.png')
+        this.load.audio('fanfare', 'assets/fanfare.wav')
     }
     
     create() {
-        this.add.image(400, 300, 'background').setScale(0.575)
+        this.fanfare = this.sound.add('fanfare').setVolume(0.3)
+        this.fanfare.play()
+        this.add.image(500, 300, 'summaryBackground').setScale(0.575)
         this.add.text(game.config.width/3, 25, 'Level ' + `${this.level}`).setFontSize(50)
         this.add.text(game.config.width/3, 75, 'Summary').setFontSize(50)
         this.add.text(game.config.width/3.5, 150, 'Time: ' + `${this.time}`).setFontSize(25)
         this.add.text(game.config.width/3.5, 175, 'Score: ' + `${this.score}`).setFontSize(25)
+        this.add.text(game.config.width/3.5, 275, 'Click anywhere to continue').setFontSize(25)
+        
+        this.input.on('pointerdown', () => {
+            this.fanfare.stop()
+            if (this.level == 1) {
+                this.scene.start('level2')
+            }
+            else if (this.level == 2) {
+                this.scene.start('level3')
+            }
+            else {
+                this.scene.start('credits')
+            }
+        });
     }
 }
 
@@ -154,10 +419,15 @@ class Credits extends Phaser.Scene {
         super('credits');
     }
 
+    preload() {
+        this.load.image('summaryBackground', 'assets/summary-background.png')
+    }
+    
     create() {
+        this.add.image(500, 300, 'summaryBackground').setScale(0.575)
         this.add.text(50, 50, "Thank you for playing!").setFontSize(50);
         this.add.text(50, 100, "Click anywhere if you'd like to restart.").setFontSize(20);
-        this.input.on('pointerdown', () => this.scene.start('intro'));
+        this.input.on('pointerdown', () => this.scene.start('level1'));
     }
 }
 
@@ -165,19 +435,18 @@ let cursors;
 
 let config = {
     type: Phaser.WEBGL,
-    width: 800,
+    width: 1000,
     height: 600,
     physics: {
         default: 'arcade',
         arcade: {
-            debug: true,
             gravity: {
                 x:0,
                 y: 0
             }
         }
     },
-    scene: [Intro, Level1, Summary]
+    scene: [Intro, Level1, Level2, Level3, Summary, Credits]
 };
 
 let game = new Phaser.Game(config);
