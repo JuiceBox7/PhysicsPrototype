c29fbfcb8d8cb0e06d618ed3b2c5f4709699e76f
JuiceBox7 <juiceinbox7@gmail.com>
2023-05-17 23:52:03 -0700

    removed delay on intro

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ class Intro extends Phaser.Scene {
         \nmini platforming test!").setFontSize(25)
         this.add.text(50,100, "Click anywhere to begin.").setFontSize(20)
         this.input.on('pointerdown', () => {
-            this.cameras.main.fade(1000, 0,0,0);
             this.time.delayedCall(1000, () => this.scene.start('level1'))
         });
     }
